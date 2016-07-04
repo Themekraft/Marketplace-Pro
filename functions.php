14a0758c4782ac4216aa5d1ea34457139125b536
konradsroka <konrads@themekraft.com>
2016-07-05 01:33:50 +1000

    buddypress member directory and profile: mobile optimisations and tons of fixes

--- functions.php
+++ functions.php
@@ -281,7 +281,7 @@ if ( class_exists( 'WooCommerce' ) ) {
     // Single Product - Header
     remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_upsell_display', 15 );
     remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_related_products', 20 );
-    remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40 );
+    // remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40 );
 
     // Remove Heading "Product Description"
     add_filter( 'woocommerce_product_description_heading', 'remove_product_description_heading' );
