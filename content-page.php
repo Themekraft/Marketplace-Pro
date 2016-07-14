2440937b3aec36fee788f0e2a5f4e3581d83b430
konradsroka <konrads@themekraft.com>
2016-07-15 04:43:00 +1000

    remove thumbnail in page single

--- content-page.php
+++ content-page.php
@@ -7,14 +7,12 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	
 	<header>
 		<h1 class="page-title"><?php the_title(); ?></h1>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
-		<div class="entry-content-thumbnail">
-			<?php the_post_thumbnail(); ?>
-		</div>
 		<?php the_content(); ?>
 		<?php
 			wp_link_pages( array(
